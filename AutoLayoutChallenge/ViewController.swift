cd2b29aaa86435be9dd2700e47dd319c49264c66
touyou <fujiyou1121@gmail.com>
2019-02-12 23:25:42 +0900

    Initial Commit

---
+++ AutoLayoutChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutChallenge
+//
+//  Created by 藤井陽介 on 2019/02/12.
+//  Copyright © 2019 touyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
